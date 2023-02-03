2ef3996d95cc4c37a64006d4f1c72b4740c4bc01
Kiara Marie <koodlem@gmail.com>
2023-02-04 03:14:27 +0800

    created endpoint routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    get '/languages' => 'languages#index'
+    get '/countries' => 'countries#index'
+    get '/holidays/:country/:year' => 'holidays#index'
+  end
 end
